ded7e96c21dd225292c3460380881aca65f0b81f
zhouchaowei <zhouchaowei96492@163.com>
2022-07-06 23:29:24 +0800

    提交物流模块服务层代码

---
+++ electronic-commerce-provider/electronic-commerce-logistics-provider/src/main/java/com/newcore/electronic/commerce/logistics/provider/ElectronicCommerceLogisticsProviderApplication.java
@@ -0,0 +1,17 @@
+package com.newcore.electronic.commerce.logistics.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 电商平台-物流服务-主启动类
+ * @author zhouchaowei
+ * @date 2022-07-06
+ */
+@SpringBootApplication
+public class ElectronicCommerceLogisticsProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicCommerceLogisticsProviderApplication.class,args);
+    }
+}
