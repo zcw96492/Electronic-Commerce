e1d9d313651b6db856fb644e2d5f781b9b2e973c
zhouchaowei <zhouchaowei96492@163.com>
2022-07-06 23:16:08 +0800

    提交商品模块服务层代码

---
+++ electronic-commerce-provider/electronic-commerce-commodity-provider/src/main/java/com/newcore/electronic/commerce/commodity/provider/ElectronicCommerceCommodityProviderApplication.java
@@ -0,0 +1,17 @@
+package com.newcore.electronic.commerce.commodity.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 电商平台-商品服务-主启动类
+ * @author zhouchaowei
+ * @date 2022-07-06
+ */
+@SpringBootApplication
+public class ElectronicCommerceCommodityProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectronicCommerceCommodityProviderApplication.class,args);
+    }
+}
